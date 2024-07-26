45c0bb32881cd30555190145c5492d900c1ee452
FazilFizzo-chaos <156065220+FazilFizzo-chaos@users.noreply.github.com>
2024-07-26 10:25:53 +0300

    Updated SayHelloController.java for html response to web
    hardcoded html code using StringBuffer

--- firstwebapp-spring/src/main/java/com/example/firstwebapp_spring/hello/SayHelloController.java
+++ firstwebapp-spring/src/main/java/com/example/firstwebapp_spring/hello/SayHelloController.java
@@ -12,4 +12,19 @@ public class SayHelloController {
     public String sayHello() {
         return "Hello! What are you learning today?";
     }
+
+    @RequestMapping("say-hello-html")
+    public String sayHelloHtml() {
+      StringBuffer stringBuffer = new StringBuffer();
+      stringBuffer.append("<html>");
+      stringBuffer.append("<head>");
+      stringBuffer.append("<title>My first HTML Page</title>");
+      stringBuffer.append("</head>");
+      stringBuffer.append("<body>");
+      stringBuffer.append("My first html page with body");
+      stringBuffer.append("</body>");
+      stringBuffer.append("</html>");
+      
+      return stringBuffer.toString();
+    }
 }
